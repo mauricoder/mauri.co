c09c6dcd24d70d3a39800a70cde86a861e75aed3
Mauricio Etcheverry <mauricio@yoreparo.com>
2021-05-01 10:46:59 -0300

    Fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Press+Start+2P'}
     ]
   },
 
